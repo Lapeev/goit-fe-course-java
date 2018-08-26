f0ff4d5af3bf90153f07518d9f88f837877dd484
lapeev16@gmail.com <lapeev16@gmail.com>
2018-08-26 17:10:54 +0300

    ver1

---
+++ homework-01/js/hw2-1.js
@@ -0,0 +1,23 @@
+'use strict';
+const adminLogin = 'admin';
+const adminPassword = 'm4ngo1zh4ackz0r';
+const alertUser = 'Отменено пользователем!';
+const alertDenied = 'Доступ запрещен!';
+const alertWelcome = 'Добро пожаловать!';
+const login = prompt('Введите логин');
+if (login === null) {
+  alert(alertUser);
+} else if (login !== adminLogin) {
+  alert(alertDenied);
+} else {
+  const pass = prompt('Введите пароль');
+  if (pass === null) {
+    alert(alertUser);
+  }
+  else if (pass !== adminPassword) {
+    alert(alertDenied);
+  }
+  else {
+    alert(alertWelcome);
+  }
+}
