6389e6867ee468f6abf8d2310c908650cbafac55
lapeev16@gmail.com <lapeev16@gmail.com>
2018-08-28 14:44:15 +0300

    ver2

--- homework-01/js/hw2-1.js
+++ homework-01/js/hw2-1.js
@@ -5,12 +5,15 @@ const alertUser = 'Отменено пользователем!';
 const alertDenied = 'Доступ запрещен!';
 const alertWelcome = 'Добро пожаловать!';
 const login = prompt('Введите логин');
+
 if (login === null) {
   alert(alertUser);
 } else if (login !== adminLogin) {
   alert(alertDenied);
 } else {
+
   const pass = prompt('Введите пароль');
+  
   if (pass === null) {
     alert(alertUser);
   }
