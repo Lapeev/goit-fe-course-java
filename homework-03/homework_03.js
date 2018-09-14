34ec8af7a7b059d1634eec40071b943a848108e5
lapeev16@gmail.com <lapeev16@gmail.com>
2018-09-15 00:50:41 +0300

    v2

--- homework-03/homework_03.js
+++ homework-03/homework_03.js
@@ -3,34 +3,32 @@ const logins = ["Mango", "robotGoogles", "Poly", "Aj4x1sBozz", "qwerty123"];
 
 const isLoginValid = function(login) {
   
-  const valid = login.length >= 4 && login.length <= 16;
+  return  login.length >= 4 && login.length <= 16;
   
-  return valid;
 };
 
 const isLoginUnique = function(allLogins, login) {
   
-  const unique = allLogins.includes(login);
+  return allLogins.includes(login);
   
-  return unique;
+  
 };
 
 const addLogin = function(login) {
-  let result;
   if (isLoginValid(login)) {
     
     if (!isLoginUnique(logins, login)) {
       logins.push(login);
-      result = 'Логин успешно добавлен!';
+      return 'Логин успешно добавлен!';
     }
     
     else {
-      result = 'Такой логин уже используется!';
+      return 'Такой логин уже используется!';
     }
     
   }
   else {
-    result = 'Ошибка! Логин должен быть от 4 до 16 символов';
+    return 'Ошибка! Логин должен быть от 4 до 16 символов';
   }
   
   return result;
