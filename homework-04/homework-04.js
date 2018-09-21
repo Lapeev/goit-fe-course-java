d0c1ca6c3fea198216091e2c7e1fc08151fdd938
lapeev16@gmail.com <lapeev16@gmail.com>
2018-09-21 17:31:08 +0300

    v2

--- homework-04/homework-04.js
+++ homework-04/homework-04.js
@@ -1,79 +1,74 @@
 'use strict';
+
 const products = {
-    bread: 10,
-    milk: 15,
-    apples: 20,
-    chicken: 50,
-    cheese: 40,
-  };
-  const order = {
-    bread: 2,
-    milk: 2,
-    apples: 1,
-    cheese: 1
-  };
+  bread: 10,
+  milk: 15,
+  apples: 20,
+  chicken: 50,
+  cheese: 40,
+};
+const order = {
+  bread: 2,
+  milk: 2,
+  apples: 1,
+  cheese: 1
+};
+
+function Cashier(name, productDatabase){
+  this.name = name;
+  this.productDatabase = productDatabase;
+  this.customerMoney = 0;
   
-  function Cashier(name, productDatabase){
-    this.name = name;
-    this.productDatabase = productDatabase;
-    this.customerMoney = 0;
+  this.getCustomerMoney = value => this.customerMoney = value;
+  
+  
+  this.countTotalPrice = order => {
+    let sum = 0;
     
-    this.getCustomerMoney = function(value = 0) {
-      this.customerMoney = value;
+    for (const product in order) {
+      sum += order[product]*this.productDatabase[product];
     }
     
-    this.countTotalPrice = function(order) {
-      let sum = 0;
-      
-      for (const product in order) {
-        sum += order[product]*this.productDatabase[product];
-      }
-      
-      this.totalPrice = sum;
-      return sum;
-    }
-    
-    this.countChange = function() {
-      return this.customerMoney - this.totalPrice;
-    }
-    
-    this.onSuccess = function() {
-      console.log(`спасибо за покупку, ваша сдача ${this.countChange()}`);
-    }
-    
-    this.onError = function() {
-      console.log('Очень жаль, вам не хватает денег на покупки');
-    }
-    
-    this.reset = function() {
-      this.customerMoney = 0; 
-      console.log('бабки: ', this.customerMoney);
-    }
-    
-    this.selling = function(value) {
-      if (value >= 0) {
-        this.onSuccess();
-      }
-      else {
-        this.onError();
-      }
-      
-      this.reset();
-    }
-    
+    return sum;
   }
   
-  const mango = new Cashier('Mango', products);
-  const totalPrice = mango.countTotalPrice(order);
+  this.countChange = () => { 
+    return (this.customerMoney - this.countTotalPrice(order) >=0 
+            ? this.customerMoney - this.countTotalPrice(order) 
+            : null);
+  }
   
-  mango.getCustomerMoney(300);
+  this.onSuccess = () => console.log(`спасибо за покупку, ваша сдача ${this.countChange()}`);
   
-  const change = mango.countChange();
+  this.onError = () => console.log('Очень жаль, вам не хватает денег на покупки');
   
-  console.log('сдача: ', change);
-  console.log('общая стоимость: ', totalPrice);
-  console.log('бабки: ', mango.customerMoney);
-  console.log('имя кассира: ', mango.name);
-  console.log(mango.productDatabase);
-  
-  mango.selling(change);
+  this.reset = () => {
+    this.customerMoney = 0; 
+    console.log('бабки: ', this.customerMoney);
+  }
+}
+
+const mango = new Cashier('Mango', products);
+const totalPrice = mango.countTotalPrice(order);
+
+mango.getCustomerMoney(300);
+
+const change = mango.countChange();
+
+console.log('сдача: ', change);
+console.log('общая стоимость: ', totalPrice);
+console.log('бабки: ', mango.customerMoney);
+console.log('имя кассира: ', mango.name);
+console.log(mango.productDatabase);
+
+if (change !== null) {
+  mango.onSuccess(change);
+}
+else {
+  mango.onError();
+}
+mango.reset();
+
+
+
+
